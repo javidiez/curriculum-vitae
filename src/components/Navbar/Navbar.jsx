b05d45db47b99f4d7bc1599e0c0d4d3accd03eba
Javier Diez <javierdiez92@gmail.com>
2024-12-16 14:34:39 +0100

    ADD nanobytes experiencie

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import "./Navbar.css"
-import javier from '../../assets/img/javier-diez.jpeg'
+import javier from '../../assets/img/jd.jpeg'
 
 export const Navbar = () => {
 
--- src/components/NavbarCv/NavbarCv.jsx
+++ src/components/NavbarCv/NavbarCv.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { SearchBoxCv } from "../SearchBoxCv/SearchBoxCv"
-import javier from '../../assets/img/javier-diez.jpeg'
+import javier from '../../assets/img/jd.jpeg'
 import './NavbarCv.css'
 import { Link } from "react-router-dom"
 import Swiper from "swiper"
